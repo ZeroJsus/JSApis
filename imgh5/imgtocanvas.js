af96b4ca6e837dee5f308979c9267bc05109014d
ZeroJsus <f723706588@qq.com>
2018-11-20 17:44:38 +0800

    update copyboard

--- imgh5/imgtocanvas.js
+++ imgh5/imgtocanvas.js
@@ -14,3 +14,26 @@ function getBase64(){
 getBase64().then(function(res){
   console.log(res)
 })
+
+
+// promise ios10.3+ 
+// function getBase64(file){
+// 	return new Promise(function(resolve, reject){
+// 		let reader = new FileReader();
+// 		reader.readAsDataURL(file);
+// 		reader.addEventListener('load', function(){
+// 			resolve(reader.result);
+// 		})
+// 		reader.addEventListener('error', function(error){
+// 			reject(error);
+// 		})
+// 	})
+// }
+
+// getBase64()
+// .then(function(base64){
+// 	console.log(base64)
+// })
+// .catch(function(error){
+// 	console.error(error)
+// })
